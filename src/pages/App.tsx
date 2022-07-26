21322bf39aa0fe6fcdbc69e27ba75b4cb5e48d33
Ricardo Milan <ricardo@fluencypass.com>
2022-07-26 10:25:01 -0300

    cleanup

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
+import Sidebar from "../components/sidebar";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
+      <Sidebar />
     </div>
   );
 }

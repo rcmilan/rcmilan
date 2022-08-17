d1b0fee63b26d83820e916f0b931c3e84d75424d
Ricardo Milan <ricardo@fluencypass.com>
2022-08-17 20:57:16 -0300

    contato

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faSpotify } from "@fortawesome/free-brands-svg-icons";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import {
+  faGithub,
+  faLinkedin,
+  faSpotify,
+} from "@fortawesome/free-brands-svg-icons";
 import Header1 from "../components/header1";
 
 interface Content {
@@ -17,7 +18,7 @@ interface Content {
 
 const buildLine = (children: JSX.Element) => {
   return (
-    <div className="m-1 p-2 hover:shadow-sm hover:shadow-black border-2 border-neutral-300 hover:border-neutral-900">
+    <div className="m-1 p-2 border-2 border-neutral-300 hover:border-neutral-900">
       {children}
     </div>
   );
@@ -33,30 +34,25 @@ const buildContent = (c: Content) => {
   return (
     <>
       <span className="inline-block align-middle">{c.icon}</span>
-      <span className="inline-block align-middle uppercase text-2xl ml-5">
+      <span className="inline-block align-middle uppercase text-right text-2xl ml-5">
         {c.description}
       </span>
     </>
   );
 };
 
-const buildIcon = (i: IconProp) => {
-  const iconSize = "3x";
-
-  return <FontAwesomeIcon icon={i} size={iconSize} />;
-};
-
+const iconSize = "3x";
 const contents: Array<Content> = [
   {
     key: 1,
-    icon: buildIcon(faGithub),
+    icon: <FontAwesomeIcon icon={faGithub} size={iconSize} />,
     available: true,
     contactURL: new URL("https://github.com/rcmilan"),
     description: "Github",
   },
   {
     key: 2,
-    icon: buildIcon(faLinkedin),
+    icon: <FontAwesomeIcon icon={faLinkedin} size={iconSize} />,
     available: true,
     contactURL: new URL(
       "https://www.linkedin.com/in/ricardo-silva-milan-044268b5/"
@@ -65,7 +61,7 @@ const contents: Array<Content> = [
   },
   {
     key: 3,
-    icon: buildIcon(faSpotify),
+    icon: <FontAwesomeIcon icon={faSpotify} size={iconSize} />,
     available: true,
     contactURL: new URL(
       "https://open.spotify.com/user/22wpi4nobprbbzofphwudzuuq"

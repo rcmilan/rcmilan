ae8e38aa083ec08099c64107d1e12a39545c118f
Ricardo Silva Milan <13418519+rcmilan@users.noreply.github.com>
2022-08-17 20:58:18 -0300

    Contact (#5)
    * icons
    
    * adicionando links
    
    * shadow
    
    * snippet
    
    * header e descrição dos links
    
    * contato
    
    * contact
    
    Co-authored-by: Ricardo Milan <ricardo@fluencypass.com>

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,15 +1,95 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faSpotify,
+} from "@fortawesome/free-brands-svg-icons";
+import Header1 from "../components/header1";
+
+interface Content {
+  key: number;
+  icon: JSX.Element;
+  available: boolean;
+  contactURL: URL;
+  description: string;
+}
+
+const buildLine = (children: JSX.Element) => {
+  return (
+    <div className="m-1 p-2 border-2 border-neutral-300 hover:border-neutral-900">
+      {children}
+    </div>
+  );
+};
+
+const buildAvailableLink = (href: string, children: JSX.Element) => (
+  <a href={href} target="_blank" rel="noreferrer">
+    {buildLine(children)}
+  </a>
+);
+
+const buildContent = (c: Content) => {
+  return (
+    <>
+      <span className="inline-block align-middle">{c.icon}</span>
+      <span className="inline-block align-middle uppercase text-right text-2xl ml-5">
+        {c.description}
+      </span>
+    </>
+  );
+};
+
+const iconSize = "3x";
+const contents: Array<Content> = [
+  {
+    key: 1,
+    icon: <FontAwesomeIcon icon={faGithub} size={iconSize} />,
+    available: true,
+    contactURL: new URL("https://github.com/rcmilan"),
+    description: "Github",
+  },
+  {
+    key: 2,
+    icon: <FontAwesomeIcon icon={faLinkedin} size={iconSize} />,
+    available: true,
+    contactURL: new URL(
+      "https://www.linkedin.com/in/ricardo-silva-milan-044268b5/"
+    ),
+    description: "LinkedIn",
+  },
+  {
+    key: 3,
+    icon: <FontAwesomeIcon icon={faSpotify} size={iconSize} />,
+    available: true,
+    contactURL: new URL(
+      "https://open.spotify.com/user/22wpi4nobprbbzofphwudzuuq"
+    ),
+    description: "Spotify",
+  },
+];
 
 function Contact() {
   const { t } = useTranslation();
+
   return (
     <>
-      <h1>{t("translation.contact.header")}</h1>
+      <Header1>{t("translation.contact.header")}</Header1>
+      <div className="flex flex-col">
+        {contents
+          .sort((c1, c2) => c1.key - c2.key)
+          .map((c) => {
+            const childrenLineContent = buildContent(c);
 
-      <div className="flex">
-        <div className="flex-1">Link 1</div>
-        <div className="flex-1">Link 1</div>
+            return (
+              <div key={c.key}>
+                {c.available
+                  ? buildAvailableLink(c.contactURL.href, childrenLineContent)
+                  : buildLine(childrenLineContent)}
+              </div>
+            );
+          })}
       </div>
     </>
   );
